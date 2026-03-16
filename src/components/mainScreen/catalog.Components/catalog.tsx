7e0823231554b479795e79a296bba714adac26d0
Mark <retrograde1779@gmail.com>
2026-03-16 10:53:02 +0300

    Изменил боковую панель - при прокрутке вниз, теперь нет белого пространства

--- src/components/mainScreen/catalog.Components/catalog.tsx
+++ src/components/mainScreen/catalog.Components/catalog.tsx
@@ -13,21 +13,31 @@ interface itemInterface{
 
 export default function Catalog({pName, children}){
     const title = useState(pName)
+    let tea_list_li = 
+    <div>
+        <h3>Здесь, пока, ничего нет</h3>
+    </div>;
 
-    const tea_list_li = children.map((item: itemInterface) => {
-        return(
-            <div className={styles.container}>
-                <div className={styles.imgTitle}>
-                    <img src={item.img} alt="" />
-                    <span>{item.title}</span>
+    if(children){
+        tea_list_li = children.map((item: itemInterface) => {
+            return(
+                <div className={styles.container}>
+                    <div className={styles.imgTitle}>
+                        <img src={item.img} alt="" />
+                        <span>{item.title}</span>
+                    </div>
+                    <div className={styles.priceDesc}>
+                        <span>Цена: {item.price}₽</span>
+                        <p>{item.description}</p>
+                    </div>
                 </div>
-                <div className={styles.priceDesc}>
-                    <span>Цена: {item.price}₽</span>
-                    <p>{item.description}</p>
-                </div>
-            </div>
-        )
-    })
+            )
+        })
+
+        // document.getElementsByClassName('container').forEach()
+    }
+
+
 
     return (
         <div className={styles.main_container}>
